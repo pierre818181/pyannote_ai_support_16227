7278c8b21ec333f5df8d13860cf475b086f81c7a
Anamika Nayak <anamika.nayak@runpod.io>
2025-05-13 05:58:41 +0000

    using pyannote/speaker-diarization as the model name

--- rp_handler.py
+++ rp_handler.py
@@ -7,7 +7,7 @@ def handler(event):
     # Extract input data
     path = event["input"].get("url")
     print(f"Creating pipeline object")
-    pipeline = Pipeline("test", batch_size=8, debug=True)
+    pipeline = Pipeline("pyannote/speaker-diarization", batch_size=8, debug=True)
     print("Diarizing audio file")
     result = pipeline.diarize(path)
     print("Diarization complete")
