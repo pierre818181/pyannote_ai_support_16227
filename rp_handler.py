30aef0069171fea61993ee68b51c9c9adbad26d1
pierre818181 <159487921+pierre818181@users.noreply.github.com>
2025-05-14 15:58:27 -0700

    Update rp_handler.py

--- rp_handler.py
+++ rp_handler.py
@@ -7,7 +7,7 @@ def handler(event):
     # Extract input data
     path = event["input"].get("url")
     print(f"Creating pipeline object")
-    pipeline = Pipeline("pyannote/speaker-diarization", batch_size=1, debug=True)
+    pipeline = Pipeline("pyannote/speaker-diarization", batch_size=24, debug=True)
     print("Diarizing audio file")
     result = pipeline.diarize(path)
     print("Diarization complete")
